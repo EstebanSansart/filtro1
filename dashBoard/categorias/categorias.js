8c794df2728c92e7769e63e636f6ca81976229e8
EstebanSansart <julianestebansanabria@gmail.com>
2023-05-16 13:37:47 -0500

    feat: :sparkles: twelveCommit
    Añadidos pequeños detalles y comienzo a intentar los cruds.

--- dashBoard/categorias/categorias.js
+++ dashBoard/categorias/categorias.js
@@ -1,10 +1,42 @@
-// (function(){
-//     document.addEventListener('DOMContentLoaded', showItems);
-//     function showItems(){
-//         console.log('DESDE IIFE')
-//     }
-// })()
+/* Form y config */
+const cate = document.querySelector('#cate')
+const ans = document.querySelector('#ans')
+const url = 'http://localhost:3000'
+const config = {
+    headers: new Headers({
+        'Content-Type': 'application/json'
+    })
+}
+/* funciones */
+const addCate= async (e)=> {
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target))
+    config.method = "POST"
+    config.body = JSON.stringify(data)
+    let info = await fetch(`${url}/categorias`, config)
+    showCate()
+}
+async function delCate(e){
+    let info = e.target.value;
+    config.method = "DELETE"
+    let peticion = await fetch(`${url}/categorias/${info}`, config)
+    showCate()
+} 
 
-let nuevoItem = document.getElementById("exampleInputUser");
-let id = 0;
+const showCate = async()=> {
+    let info = await fetch(`${url}/categorias`)
+    let data = await info.json()
+    data.forEach(element => {
+        let plantilla=`
+        <tr>
+        <th>${element.id}</th>
+        <th>${element.categoria}</th>
+        <th><button type="button" id="delete" onclick="delCate(event)" value="${element.id}" class="btn btn-danger">Borrar</button></th></tr>`
+        ans.innerHTML +=  plantilla
+    });
+}
 
+
+
+showCate()
+cate.addEventListener('submit', addCate)
